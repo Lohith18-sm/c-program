103c0570bf202c9426dbdc73ba640d701f58583d
LOHITH S.M <lohith.s.m.032006@gmail.com>
2025-07-30 00:21:01 +0530

    Delete q31.c

--- q31.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-   printf("Enter a number (1 to 7): ");
-    int n; scanf("%d", &n);
-    char * d[] = {"Invalid","Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
-    printf("%s\n", (n>0&&n<8)?d[n]:"Invalid");
-    return 0;
-}
